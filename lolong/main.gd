604ec607b6fa1182f55be84dc4b7226267182f1d
Lolong,Clariz joy <157397194+yumhae@users.noreply.github.com>
2026-05-08 12:03:43 +0800

    Add files via upload

---
+++ lolong/main.gd
@@ -0,0 +1,44 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+var score
+
+func _ready():
+	pass
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+
+func new_game():
+	get_tree().call_group("mobs", "queue_free")
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	mob.position = mob_spawn_location.position
+	var direction = mob_spawn_location.rotation + PI / 2
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	add_child(mob)
+
+func _on_hud_start_game() -> void:
+	new_game()
---
+++ lolong/mob.gd
@@ -0,0 +1,9 @@
+extends RigidBody2D
+
+func _ready():
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
